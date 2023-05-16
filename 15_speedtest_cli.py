5b1609784c579e39fccbf1a5bcb99341014a0dba
Ali Rios <ali.rios@gmail.com>
2023-05-16 20:41:15 -0300

    code for webbrowser

---
+++ 15_speedtest_cli.py
@@ -0,0 +1,12 @@
+
+# import speedtest
+
+# d_speed = speedtest.Speedtest()
+
+# print(f'{d_speed.download()/8000000:.2f}mb')
+
+import speedtest
+
+u_speed = speedtest.Speedtest()
+
+print(f'{u_speed.upload()/8000000:2f}mb')
---
+++ 18_open_website.py
@@ -0,0 +1,10 @@
+
+import webbrowser
+
+url = 'www.google.com'
+
+open_web_browser = webbrowser.open(url)
+print(open_web_browser)
+
+
+open_web = webbrowser.open_new_tab(url)
