529dbb4639ea46d3214050f62e081afd2c0ac065
Ali Rios <ali.rios@gmail.com>
2023-05-20 17:23:08 -0300

    code with enumerate

---
+++ 26_list_tuples_with_enumerate.py
@@ -0,0 +1,8 @@
+
+days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+# days_tuples = list(enumerate(days, start=1))
+
+# days_tuples = [(enumerate(day, start=1)) for day in days]
+
+print(days_tuples)
