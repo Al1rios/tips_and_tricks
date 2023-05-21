40c98f2ed852fbb2ddf4ed228a9955c14ea7d9b8
Ali Rios <ali.rios@gmail.com>
2023-05-21 19:11:32 -0300

    code colors

---
+++ 28_print_colored_text.py
@@ -0,0 +1,13 @@
+
+
+class Color():
+    Black = '\033[30m'
+    Green = '\033[32m'
+    Blue = '\033[34m'
+    Magenta = '\033[35m'
+    Red = '\033[31m'
+    Cyan = '\033[36m'
+    White = '\033[37m'
+    Yellow = '\033[33m'
+
+print(f'{Color.Red} Warning: {Color.White}  ')
