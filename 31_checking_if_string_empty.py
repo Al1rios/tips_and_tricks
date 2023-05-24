bb19b40e1b13d377002c3a23c64b2bfec5c16c8e
Ali Rios <ali.rios@gmail.com>
2023-05-23 21:22:44 -0300

    code reduce list

---
+++ 31_checking_if_string_empty.py
@@ -0,0 +1,15 @@
+
+str1 = ''
+
+if not str1:
+    print('this string is empty')
+else:
+    print('this string is NOT empty')
+
+
+str2 = 'string'
+
+if not str2:
+    print('this string is empty')
+else:
+    print('this string NOT empty')
