e52fd78798bb4090b2a1499453b5a745259b95e2
Ali Rios <ali.rios@gmail.com>
2023-06-05 20:46:59 -0300

    selenium and difference two list

---
+++ 63_substring_of_string.py
@@ -0,0 +1,14 @@
+
+s = 'Please find something you like'
+
+# if 'like' in s:
+#     print('like is a substring of s')
+# else:
+#     print('like is not a substring of s')
+
+if "like" not in s:
+    print('like is not a substring of s')
+else:
+    print('like is a substring of s')
+
+print(s.find('something'))
