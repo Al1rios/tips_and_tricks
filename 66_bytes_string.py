cab02cdc882b8d60f7f445ffda12ad60fa259f2c
Ali Rios <ali.rios@gmail.com>
2023-06-06 20:25:40 -0300

    multiple data, sorting

---
+++ 66_bytes_string.py
@@ -0,0 +1,8 @@
+
+s = b'love for life'
+print(type(s))
+str1 = str(s, 'utf-8')
+print(str1)
+
+str2 = s.decode()
+print(str2)
---
+++ 67_multiple_input_from_user.py
@@ -0,0 +1,11 @@
+
+a, b, c = input("Please input 3 numbers: ").split(sep=',')
+
+average = (int(a) + int(b) + int(c)) / 3
+
+print(average)
+
+n = [nam for nam in input('Please multiple names: ').split(sep=',')]
+
+print(n)
+
